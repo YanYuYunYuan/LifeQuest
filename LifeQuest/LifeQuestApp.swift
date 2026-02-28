78f60f301f5c765ee20e98aaed7f9eb979ca933e
zwy168 <yuanyuyunyuan1314@gmail.com>
2026-02-28 17:30:04 +0800

    Initial Commit

---
+++ LifeQuest/LifeQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeQuestApp.swift
+//  LifeQuest
+//
+//  Created by Mac on 2026/2/28.
+//
+
+import SwiftUI
+
+@main
+struct LifeQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
